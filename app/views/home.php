237351321f7c3e58b2575a603b75ef31b19fd554
Lucas Gomes <58189878+lucasffgomes@users.noreply.github.com>
2023-01-18 02:57:53 -0300

    finished home view

---
+++ app/views/home.php
@@ -0,0 +1,31 @@
+<header>
+    <div class="container mx-auto">
+        <div class="navbar bg-base-100">
+            <div class="flex-1">
+                <a href="/" class="btn btn-ghost normal-case text-xl">Teste PHP</a>
+            </div>
+            <div class="flex-none">
+                <ul class="menu menu-horizontal px-1">
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/listar/produtos">Listar produtos</a></li>
+                    <li><a href="/listar/fornecedores">Listar fornecedores</a></li>
+                    <li><a href="/adicionar/fornecedor">Adicionar produto</a></li>
+                    <li><a href="/adicionar/produto">Adicionar fornecedor</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</header>
+
+<main>
+    <section class="container mx-auto">
+        <div class="hero h-[calc(100vh-10rem)]">
+            <div class="hero-content text-center">
+                <div class="max-w-md">
+                    <h1 class="text-4xl font-bold">Bem vindo ao CRUD</h1>
+                    <p class="py-6">Esta é uma implementação simples das 4 operações básicas em um banco de dados: <strong>C</strong>reate, <strong>R</strong>ead, <strong>U</strong>pdate e <strong>D</strong>elete.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
