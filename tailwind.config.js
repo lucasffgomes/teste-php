73916d4d2af7e610714fba32098e39ba34d70a61
Lucas Gomes <58189878+lucasffgomes@users.noreply.github.com>
2023-01-18 02:47:13 -0300

    configured daisyui for components

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./app/views/*.php"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        lato: ['Lato', 'sans-serif']
+      }
+    },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: false,
+  },
 }
