a38cf8405360859c08e2be5b89fee53dff67fdf7
Lucas Gomes <58189878+lucasffgomes@users.noreply.github.com>
2023-01-18 16:08:19 -0300

    added where function

--- app/database/fetch.php
+++ app/database/fetch.php
@@ -27,6 +27,47 @@ function read(string $table, string $fields = '*')
 }
 
 /**
+ * Função responsável por filtrar registros retornados de uma consulta ao banco.
+ */
+function where()
+{
+    global $query;
+
+    if (isset($query['where'])) {
+        throw new Exception('Verifique quantos WHEREs estão sendo chamados ao criar a query');
+    }
+
+    $args = func_get_args();
+    $numArgs = func_num_args();
+
+    // VERIFICA SE O READ() ESTÁ SENDO CHAMADO
+    if (!isset($query['read'])) {
+        throw new Exception('Antes de chamar o where() chame o read().');
+    }
+
+    // VERIFICA SE A QUANTIDADE DE ARGUMENTOS PASSADOS É MENOR QUE 2 OU MAIOR QUE 3
+    if ($numArgs < 2 || $numArgs > 3) {
+        throw new Exception('O where() precisa de 2 ou 3 parâmetros.');
+    }
+
+    if ($numArgs === 2) {
+        $field = $args[0];
+        $operator = '=';
+        $value = $args[1];
+    }
+
+    if ($numArgs === 3) {
+        $field = $args[0];
+        $operator = $args[1];
+        $value = $args[2];
+    }
+
+    $query['where'] = true;
+    $query['execute'] = array_merge($query['execute'], [$field => $value]);
+    $query['sql'] = "{$query['sql']} WHERE {$field} {$operator} :{$field}";
+}
+
+/**
  * Função responsável por executar qualquer QUERY no banco de dados.
  */
 function execute(bool $isFetchAll = true, bool $rowCount = false)
