fd21b0e7d7fc9c9f722af9013c57650f7af243d5
Lucas Gomes <58189878+lucasffgomes@users.noreply.github.com>
2023-01-18 03:48:40 -0300

    added fornecedor controller

--- app/controllers/Fornecedor.php
+++ app/controllers/Fornecedor.php
@@ -6,10 +6,15 @@ class Fornecedor
 {
     public function index()
     {
+        // FORNECEDORES
+        read('fornecedor');
+        $fornecedores = execute();
+
         return [
             'view' => 'listar-fornecedores.php',
             'data' => [
                 'title' => 'Home',
+                'fornecedores' => $fornecedores
             ]
         ];
     }
