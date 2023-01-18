510a932c7c68b284e4cc898c5b3576c7dbd7ae0d
Lucas Gomes <58189878+lucasffgomes@users.noreply.github.com>
2023-01-18 15:25:00 -0300

    finished fornecedor controller

--- app/controllers/Fornecedor.php
+++ app/controllers/Fornecedor.php
@@ -13,9 +13,51 @@ class Fornecedor
         return [
             'view' => 'listar-fornecedores.php',
             'data' => [
-                'title' => 'Home',
+                'title' => 'Listar fornecedores - Teste PHP',
                 'fornecedores' => $fornecedores
             ]
         ];
+    }
+
+    public function create()
+    {
+        var_dump($_SESSION);
+
+        return [
+            'view' => 'adicionar-fornecedor.php',
+            'data' => [
+                'title' => 'Adicionar fornecedor - Teste PHP',
+            ]
+        ];
+    }
+
+    public function store()
+    {
+        $validate = validate([
+            'nome_vendedor'     => 'required',
+            'email_vendedor'    => 'required',
+            'cnpj'              => 'required',
+            'razao_social'      => 'required',
+            'nome_fantasia'     => 'required',
+            'telefone'          => 'required',
+            'celular_vendedor'  => 'required',
+        ], persistInputs: true);
+
+        // SE A VALIDAÇÃO NÃO PASSAR, REDIRECIONA PARA PREENCHER O FORMULÁRIO NOVAMENTE
+        if (!$validate) {
+            return redirect('/adicionar/fornecedor');
+        }
+
+        $created = create('fornecedor', $validate);
+
+        // SE NÃO CADASTROU
+        if (!$created) {
+            setFlash('message', 'Ocorreu um erro ao cadastrar, tente novamente em instantes.');
+            return redirect('/adicionar/fornecedor');
+        }
+
+        deleteOld('old');
+
+        return redirect('/listar/fornecedores');
     }
 }
