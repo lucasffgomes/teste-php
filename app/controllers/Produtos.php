cfb850c2dcf7c15651fa232e4e2b3a5551cd444e
Lucas Gomes <58189878+lucasffgomes@users.noreply.github.com>
2023-01-19 03:41:15 -0300

    added controller products

---
+++ app/controllers/Produtos.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace app\controllers;
+
+class Produtos
+{
+    public function destroy()
+    {
+        foreach(array_keys($_POST) as $itemId){
+            $deleted = delete('produto',['id_produto' => $itemId]);
+        }
+
+        if (!$deleted) {
+            setFlash('message', 'Ocorreu um erro ao deletar, tente novamente em breve');
+            return redirect('/listar/produtos');
+        }
+
+        return redirect('/listar/produtos');
+    }
+}
