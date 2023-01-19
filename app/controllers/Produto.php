555869ace05f0aa5ae892d41edad5ed46e87ea3c
Lucas Gomes <58189878+lucasffgomes@users.noreply.github.com>
2023-01-19 00:32:27 -0300

    added produto controller

---
+++ app/controllers/Produto.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace app\controllers;
+
+class Produto
+{
+    public function index()
+    {
+        // PRODUTOS
+        read('produto');
+        $produtos = execute();
+
+        return [
+            'view' => 'listar-produtos.php',
+            'data' => [
+                'title' => 'Listar produtos - Teste PHP',
+                'produtos' => $produtos
+            ]
+        ];
+    }
+
+    public function create()
+    {
+        return [
+            'view' => 'adicionar-produto.php',
+            'data' => [
+                'title' => 'Adicionar produto - Teste PHP',
+            ]
+        ];
+    }
+
+    public function store()
+    {
+        $validate = validate([
+            'nome_produto'          => 'required',
+            'valor_produto'         => 'required',
+            'peso'                  => 'required',
+            'quantidade_estoque'    => 'required'
+        ], persistInputs: true);
+
+        // SE A VALIDAÇÃO NÃO PASSAR, REDIRECIONA PARA PREENCHER O FORMULÁRIO NOVAMENTE
+        if (!$validate) {
+            return redirect('/adicionar/produto');
+        }
+
+        $created = create('produto', $validate);
+
+        // SE NÃO CADASTROU
+        if (!$created) {
+            setFlash('message', 'Ocorreu um erro ao cadastrar, tente novamente em instantes.');
+            return redirect('/adicionar/produto');
+        }
+
+        deleteOld('old');
+
+        return redirect('/listar/produtos');
+    }
+
+    public function edit($params)
+    {
+        read('produto');
+        where('id_produto', $params['editar']);
+        $produto = execute();
+
+        return [
+            'view' => 'editar-produto.php',
+            'data' => [
+                'title' => 'Editar produto - Teste PHP',
+                'produto' => $produto
+            ]
+        ];
+    }
+
+    public function update($params)
+    {
+        $validate = validate([
+            'nome_produto'          => 'required',
+            'valor_produto'         => 'required',
+            'peso'                  => 'required',
+            'quantidade_estoque'    => 'required'
+        ], persistInputs: true);
+
+        if (!$validate) {
+            return redirect('/produto/editar' . $params['editar']);
+        }
+
+        $updated = update('produto', $validate, ['id_produto' => $params['editar']]);
+
+        if (!$updated) {
+            setFlash('message', 'Ocorreu um erro ao editar, tente novamente em breve');
+            return redirect('/produto/editar' . $params['editar']);
+        }
+
+        return redirect('/listar/produtos');
+    }
+
+    public function destroy($params)
+    {
+        $deleted = delete('produto', ['id_produto' => $params['deletar']]);
+
+        if (!$deleted) {
+            setFlash('message', 'Ocorreu um erro ao deletar, tente novamente em breve');
+            return redirect('/listar/produtos');
+        }
+
+        return redirect('/listar/produtos');
+    }
+}
